29ffc807c463dd53baf8916bedd63dfaec56539a
Maximiliano Silva <silvamaxigaston@gmail.com>
2018-03-07 17:56:02 -0300

    Ajustes de estilos

--- src/menu/components/menu.js
+++ src/menu/components/menu.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
 import Logo from '../../logo/components/logo'
-import './logo.css'
+import './menu.css'
 
 const Menu = (props) => {
   return (
-    <Navbar fluid className='Logo'>
+    <Navbar fluid className='Menu'>
       <Navbar.Header>
         <Navbar.Brand>
           <a href='#home' className='Logo-isologo'>
